be8e2b42786134119b7d93d2394234b3d6545d0e
Rishabh <rishuvisionworld@gmail.com>
2025-05-22 10:45:26 +0530

    add file

---
+++ pages/Model Metrics.py
@@ -0,0 +1,37 @@
+import streamlit as st 
+
+
+st.title("Model Analysis ")
+
+
+    
+    
+st.header("Model Details ",divider=True)
+st.markdown("- Model has Accuracy of 91%")
+st.markdown("- Model is trained with a simple shallow convolutional network")
+st.download_button(
+    label="Download Model",
+    data="./smile.hdf5",
+    file_name="smile_model.hdf5",
+    icon=":material/download:",
+)
+
+image1 =  "./media/smile_conv.png"
+image2  = "./media/model_plotting.png"
+
+
+col1,col2  = st.columns(2)
+
+
+with col1:
+    st.header("Model Architecture",divider=True)
+    st.image(image1)
+
+with col2:
+    st.header("Model Progress",divider=True)
+    st.image(image2)
+    
+
+    
+    
+
